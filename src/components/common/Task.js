4612469b709dbf536bc2bfb4a476311def51a94b
xuwei <email.xuwei@gmail.com>
2020-10-23 11:29:00 +1100

    updated design on Task to be more robust with mobile screens

--- src/components/common/Task.js
+++ src/components/common/Task.js
@@ -1,4 +1,4 @@
-import { Checkbox, Box, Typography, TextField } from '@material-ui/core'
+import { Hidden, Checkbox, Box, Typography, TextField } from '@material-ui/core'
 import React, { useState, useEffect } from 'react'
 import { StateOfTask } from '../model/TaskModel'
 import EditIcon from '@material-ui/icons/Edit'
@@ -22,7 +22,7 @@ function Task(props) {
 
     const defaultProps = {
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
         p: 1,
         m: 0
     }
@@ -31,57 +31,75 @@ function Task(props) {
         case StateOfTask.Pending:
             return(
                 <Box {...defaultProps} borderBottom={BorderStyle}>
-                    <Box p={1}>
-                        <Checkbox checked={false} onChange={props.checkEvent} onClick={props.toggleTaskState}/>
+                    <Box display="flex" flexDirection="row">
+                        <Hidden smDown>
+                            <Box p={1}>
+                                <Checkbox checked={false} onChange={props.checkEvent} onClick={props.toggleTaskState}/>
+                            </Box>
+                        </Hidden>
+                        <Box p={1}>
+                            <Typography  variant="h4" color="primary">{model.descriptions}</Typography>
+                        </Box>
                     </Box>
-                    <Box p={1}>
-                        <Typography  variant="h4" color="primary">{model.descriptions}</Typography>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton onClick={props.startEdit}><EditIcon/></IconButton>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton onClick={props.doneTask}><DoneIcon/></IconButton>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton onClick={props.archiveTask}><ArchiveIcon/></IconButton>
+                    <Box display="flex" flexDirection="row" justifyContent="flex-end">
+                        <Box p={1}>
+                            <IconButton onClick={props.startEdit}><EditIcon fontSize="small"/></IconButton>
+                        </Box>
+                        <Box p={1}>
+                            <IconButton onClick={props.doneTask}><DoneIcon fontSize="small"/></IconButton>
+                        </Box>
+                        <Box p={1}>
+                            <IconButton onClick={props.archiveTask}><ArchiveIcon fontSize="small"/></IconButton>
+                        </Box>
                     </Box>
                 </Box>
             )
         case StateOfTask.Edit:
             return(
                 <Box {...defaultProps} borderBottom={BorderStyle}>
-                    <Box p={1}>
-                        <Checkbox disabled checked={false} onChange={props.checkEvent}/>
+                    <Box display="flex" flexDirection="row">
+                        <Hidden smDown>
+                            <Box p={1}>
+                                <Checkbox disabled checked={false} onChange={props.checkEvent}/>
+                            </Box>
+                        </Hidden>
+                        <Box p={1} width="90%">
+                            <TextField fullWidth={true} defaultValue={model.descriptions} onChange={handleTextChange}/>
+                        </Box >
                     </Box>
-                    <Box p={1}>
-                        <TextField defaultValue={model.descriptions} onChange={handleTextChange}/>
-                    </Box >
-                    <Box p={1}>
-                        <IconButton onClick={() => props.endEdit(model.id, desc)}><EditIcon/></IconButton>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton disabled={true} onClick={props.doneTask}><DoneIcon/></IconButton>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton disabled={true} onClick={props.archiveTask}><ArchiveIcon/></IconButton>
+                    <Box display="flex" flexDirection="row" justifyContent="flex-end">
+                        <Box p={1}>
+                            <IconButton onClick={() => props.endEdit(model.id, desc)}><EditIcon fontSize="small"/></IconButton>
+                        </Box>
+                        <Box p={1}>
+                            <IconButton disabled={true} onClick={props.doneTask}><DoneIcon fontSize="small"/></IconButton>
+                        </Box>
+                        <Box p={1}>
+                            <IconButton disabled={true} onClick={props.archiveTask}><ArchiveIcon fontSize="small"/></IconButton>
+                        </Box>
                     </Box>
                 </Box>
             )
         case StateOfTask.Done:
             return ( 
                 <Box {...defaultProps} borderBottom={BorderStyle}>
-                    <Box p={1}>
-                        <Checkbox color="primary" checked={true} onClick={props.toggleTaskState}/>
+                    <Box display="flex" flexDirection="row">
+                        <Hidden smDown>
+                            <Box p={1}>
+                                <Checkbox color="primary" checked={true} onClick={props.toggleTaskState}/>
+                            </Box>
+                        </Hidden>
+                        <Box p={1}>
+                            <Typography variant="h4" style={{textDecoration : "line-through"}}>{model.descriptions}</Typography>
+                        </Box>
                     </Box>
-                    <Box p={1}>
-                        <Typography variant="h4" style={{textDecoration : "line-through"}}>{model.descriptions}</Typography>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton onClick={props.undoTask}><UndoIcon/></IconButton>
-                    </Box>
-                    <Box p={1}>
-                        <IconButton onClick={props.archiveTask}><ArchiveIcon/></IconButton>
+                    <Box display="flex" flexDirection="row" justifyContent="flex-end">
+                        <Box p={1}>
+                            <IconButton onClick={props.undoTask}><UndoIcon fontSize="small"/></IconButton>
+                        </Box>
+                        <Box p={1}>
+                            <IconButton onClick={props.archiveTask}><ArchiveIcon fontSize="small"/></IconButton>
+                        </Box>
                     </Box>
                 </Box>
             )
