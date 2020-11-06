b05b444ab579d4fdb739dfd49e936fcfa1296683
xuwei <email.xuwei@gmail.com>
2020-11-06 23:30:35 +1100

    updated copy

--- src/components/page/HomePage.js
+++ src/components/page/HomePage.js
@@ -123,20 +123,20 @@ function HomePage() {
                         <Paper variant="outlined">
                             <Box px={LargePadding.PY}>
                                 <Box py={LargePadding.PY}>
-                                    <Typography color="primary" variant="h6">Do you see my todo items in the background?</Typography>
-                                    <Typography variant="h6">No. We have very strict protocol to protect our user's privacy and data is stored in encrypted format on our infrastructure. Your todo items will never be visible to anyone.</Typography>
+                                    <Typography color="primary" variant="body1">Do you see my todo items in the background?</Typography>
+                                    <Typography variant="body1">No. We have very strict protocol to protect our user's privacy and data is stored in encrypted format on our infrastructure. Your todo items will never be visible to anyone.</Typography>
                                 </Box>
                                 <Box py={LargePadding.PY}>
-                                    <Typography color="primary" variant="h6">What's the difference between a bunch of other todo softwares?</Typography>
-                                    <Typography variant="h6">Our mission is to design software that is extremely minimalistic. Our goal is to bring back your motivation and zen energy.</Typography>
+                                    <Typography color="primary" variant="body1">What's the difference between a bunch of other todo softwares?</Typography>
+                                    <Typography variant="body1">Our mission is to design software that is efficient and minimalistic. Our goal is to bring back your productivity, motivation and zen energy.</Typography>
                                 </Box>
                                 <Box pb={LargePadding.PY}>
-                                    <Typography color="primary" variant="h6">Is there going to be more features soon?</Typography>
-                                    <Typography variant="h6">Yes. We are building outside-the-box features for our users in upcoming weeks. However, we are not going to build all the missing todo app features that's exists in a million other todo apps.</Typography>
+                                    <Typography color="primary" variant="body1">Is there going to be more features soon?</Typography>
+                                    <Typography variant="body1">Yes. We have a roadmap to release fun and niche features for our users. We are not going to build all the missing features default to numerous other todo apps.</Typography>
                                 </Box>
                                 <Box pb={LargePadding.PY}>
-                                    <Typography color="primary" variant="h6">Is there going to be a mobile app?</Typography>
-                                    <Typography variant="h6">Not right now. However you can install todocloud as a PWA app on you phone following the video below.</Typography>
+                                    <Typography color="primary" variant="body1">Is there going to be a mobile app?</Typography>
+                                    <Typography variant="body1">Not right now. However you can install todocloud.io as a PWA app on you phone following the video below.</Typography>
                                 </Box>
                                 <Box pb={LargePadding.PY}>
                                     <Vimeo video="476199642" autoplay={false} mx="auto"/>
